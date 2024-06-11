d16a12d9e1ccacca8cb3bba4a9bfe33cf15aae0b
Olya <olya@Air--Olya.N1>
2024-06-11 20:19:44 +0200

    change of function 'handleSubmit'. Add loader

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Header from "./components/Header/Header";
 
 const App = () => {
   return (
-    <>
-      <main className={styles.mainBackground}>
-        <Header />
-        <section className={styles.mainShadow}>
-          <DisplayCalculation />
-        </section>
-      </main>
-    </>
+    <main className={styles.mainBackground}>
+      <Header />
+      <section className={styles.mainShadow}>
+        <DisplayCalculation />
+      </section>
+    </main>
   );
 };
 
--- src/components/DisplayCalculation/Form/Form.jsx
+++ src/components/DisplayCalculation/Form/Form.jsx
@@ -1,54 +1,40 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import styles from "./Form.module.css";
 import { getCurrencies } from "../../../services/getCurrencies";
 
 const Form = () => {
-  const [amount, setAmount] = useState("");
-  const [currency, setCurrency] = useState("EUR");
   const [error, setError] = useState("");
-  const [result, setResult] = useState("");
-  const [submited, setSubmited] = useState(false);
-
-  const handleChange = (event) => {
-    const newAmount = event.target.value;
-    setAmount(newAmount);
-    if (newAmount === "") {
-      setResult("");
-    }
-  };
+  const [result, setResult] = useState("0.00");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    setSubmited(true);
+    const amount = event.target.elements.amount.value;
+    const currency = event.target.elements.currency.value;
+
+    try {
+      setLoading(true);
+      const data = await getCurrencies(currency);
+      if (!isDataValid(data)) {
+        setError("Sorry, something went wrong. Please try again later.");
+        return;
+      }
+      setError("");
+
+      const rate = data.rates[0].mid;
+      const calculatedResult = (amount * rate).toFixed(2);
+      setResult(calculatedResult);
+    } catch (error) {
+      console.error(error);
+      setError("Sorry, something went wrong. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
   };
 
   const isDataValid = (data) => {
     return data?.rates?.length > 0 && data.rates[0].mid;
   };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getCurrencies(currency);
-        if (!isDataValid(data)) {
-          setError("Sorry, something went wrong. Please try again later.");
-          return;
-        }
-        setError("");
-
-        const rate = data.rates[0].mid;
-        const calculatedResult = (amount * rate).toFixed(2);
-        setResult(calculatedResult);
-      } catch {
-        setError("Sorry, something went wrong. Please try again later.");
-      }
-    };
-
-    if (submited) {
-      fetchData();
-      setSubmited(false);
-    }
-  }, [submited, currency, amount]);
 
   return (
     <form onSubmit={handleSubmit} id="form" className={styles.form}>
@@ -61,28 +47,26 @@ const Form = () => {
             name="amount"
             min="0.01"
             step="0.01"
-            placeholder="AMOUNT"
-            required
-            onChange={handleChange}
+            placeholder="Enter amount"
           />
           <select
             name="currency"
             id="currency"
             className={styles.selectCurrency}
-            onChange={(event) => {
-              setCurrency(event.target.value);
-            }}
           >
             <option value="EUR">EUR</option>
             <option value="USD">USD</option>
             <option value="CHF">CHF</option>
           </select>
         </div>
+        {loading ? (
+          <div className={styles.loader}></div>
+        ) : (
+          <span className={styles.equalSign}>&#61;</span>
+        )}
         <div className={styles.currencyBoxes}>
-          <div id="result" className={styles.result}>
-            {result}
-          </div>
-          <div className={styles.defaultCurrency}>PLN</div>
+          {result}
+          <span className={styles.defaultCurrency}>PLN</span>
         </div>
       </div>
       <button id="btn-convert" className={styles.btnConvert}>
@@ -92,4 +76,5 @@ const Form = () => {
     </form>
   );
 };
+
 export default Form;
